96c302edf6dacc7ebdbb30c3f62f584d27004323
hernancerda1 <hernan.cerda.lombardi@gmail.com>
2026-09-25 00:42:46 -0300

    Refactor app.py for better organization
    Refactor app.py to improve structure and readability.

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 resend.api_key = os.environ.get('RESEND_API_KEY')
 
 # --- CREDENCIALES DE TELEGRAM (Desde variables de entorno) ---
-TELEGRAM_BOT_TOKEN = os.environ.get('8893928560:AAEvg1UkIZsN6mNcoXEQZ1xMu1dMVGxQOL0')
-TELEGRAM_CHAT_ID = os.environ.get('7798074673')
+TELEGRAM_BOT_TOKEN = os.environ.get'8893928560:AAEvg1UkIZsN6mNcoXEQZ1xMu1dMVGxQOL0'
+TELEGRAM_CHAT_ID = os.environ.get'7798074673'
 
 # Base de datos temporal en memoria
 solicitudes_db = {}
